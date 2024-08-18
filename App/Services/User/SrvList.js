8c6e8f541d203c3f586789a3a0f548dc37f33ee9
SunilRathod27 <shrathod515@gmail.com>
2024-08-18 13:41:01 +0530

    change in images save logic

--- App/Services/User/SrvList.js
+++ App/Services/User/SrvList.js
@@ -5,6 +5,87 @@ const fs = require('fs');
 const path = require('path');
 
 module.exports = {
+    // submitForm: async function (formData) {
+    //     try {
+    //         const checkDuplicates = async (field, value) => {
+    //             const user = await RAR.User.findOne({ where: { [field]: value } });
+    //             return user ? `આ ${field} પહેલાથી જ ઉપયોગમાં છે. કૃપા કરીને અન્ય એક અજમાવવો.` : null;
+    //         };
+    
+    //         const emailMessage = await checkDuplicates('email', formData.email);
+    //         console.log('emailMessage', emailMessage);
+            
+    //         if (emailMessage) return { statusCode: 400, message: emailMessage, result: null };
+    
+    //         const whatsappMessage = await checkDuplicates('whatsapp', formData.whatsapp);
+    //         if (whatsappMessage) return { statusCode: 400, message: whatsappMessage, result: null };
+    
+    //         const registrationId = await this.generateUniqueRegistrationId();
+    
+    //         const saveImage = async (base64String, fileName) => {
+    //             const base64Data = base64String.replace(/^data:image\/\w+;base64,/, '');
+    //             const buffer = Buffer.from(base64Data, 'base64');
+    //             const filePath = path.join(__dirname, '../../../public/uploads', fileName);
+    //             await fs.promises.writeFile(filePath, buffer);
+    //             return fileName;
+    //         };
+    
+    //         let idProofFilePath = 'NA';
+    //         let photoFilePath = 'NA';
+    
+    //         if (formData.idProofPreview) {
+    //             idProofFilePath = await saveImage(formData.idProofPreview, `${registrationId}_1.jpg`);
+    //         }
+    
+    //         if (formData.photoPreview) {
+    //             photoFilePath = await saveImage(formData.photoPreview, `${registrationId}_2.jpg`);
+    //         }
+    
+    //         const newUser = {
+    //             activationId: '',
+    //             name: formData.name,
+    //             fathername: formData.fathername,
+    //             surname: formData.surname,
+    //             email: formData.email,
+    //             birthday: formData.birthday,
+    //             address: formData.address,
+    //             stateId: formData.state,
+    //             districtId: formData.district,
+    //             whatsapp: formData.whatsapp,
+    //             idProof: idProofFilePath,
+    //             photo: photoFilePath,
+    //             bloodGroupId: formData.bloodGroup,
+    //             designation: formData.designation,
+    //             registrationId: registrationId,
+    //             status: 'inactive',
+    //         };
+    
+    //         const createUser = await RAR.User.create(newUser);
+    
+    //         if (createUser && createUser.id) {
+    //             return {
+    //                 userID: createUser.id,
+    //                 statusCode: 200,
+    //                 message: `RAR ફાઉન્ડેશન સાથે જોડાવા માટે આપનો આભાર! ફોર્મ સફળતાપૂર્વક સબમિટ કરવામાં આવ્યું છે. RAR ફાઉન્ડેશન ટીમ ટૂંકમાં તમારો સંપર્ક કરશે. તમારું સક્રિયકરણ આઈડી છે: ${registrationId}.`,
+    //                 result: null
+    //             };
+    //         } else {
+    //             return {
+    //                 statusCode: 400,
+    //                 message: 'ફોર્મ સબમિટ કરવામાં ભૂલ આવી. કૃપા કરીને ફરીથી પ્રયત્ન કરો.',
+    //                 result: null
+    //             };
+    //         }
+    //     } catch (error) {
+    //         console.log("Error In Submit Form: " + error);
+    //         return {
+    //             statusCode: 500,
+    //             message: 'ફોર્મ સબમિટ કરવામાં ભૂલ આવી. કૃપા કરીને ફરીથી પ્રયત્ન કરો.',
+    //             result: null
+    //         };
+    //     }
+    // },    
+
     submitForm: async function (formData) {
         try {
             const checkDuplicates = async (field, value) => {
@@ -22,11 +103,10 @@ module.exports = {
     
             const registrationId = await this.generateUniqueRegistrationId();
     
-            const saveImage = async (base64String, fileName) => {
+            const saveImage = (base64String, fileName) => {
                 const base64Data = base64String.replace(/^data:image\/\w+;base64,/, '');
-                const buffer = Buffer.from(base64Data, 'base64');
                 const filePath = path.join(__dirname, '../../../public/uploads', fileName);
-                await fs.promises.writeFile(filePath, buffer);
+                RAR.FS.writeFileSync(filePath, base64Data, { encoding: 'base64' });
                 return fileName;
             };
     
@@ -34,11 +114,11 @@ module.exports = {
             let photoFilePath = 'NA';
     
             if (formData.idProofPreview) {
-                idProofFilePath = await saveImage(formData.idProofPreview, `${registrationId}_1.jpg`);
+                idProofFilePath = saveImage(formData.idProofPreview, `${registrationId}_1.jpg`);
             }
     
             if (formData.photoPreview) {
-                photoFilePath = await saveImage(formData.photoPreview, `${registrationId}_2.jpg`);
+                photoFilePath = saveImage(formData.photoPreview, `${registrationId}_2.jpg`);
             }
     
             const newUser = {
@@ -84,9 +164,9 @@ module.exports = {
                 result: null
             };
         }
-    },    
-
-  generateUniqueRegistrationId: async function () {
+    },
+    
+    generateUniqueRegistrationId: async function () {
     let isUnique = false;
     let registrationId;
 
